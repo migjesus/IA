816bdd58d8631e3792134adedcf7441d6c93dbbc
migjesus <migjesus21@gmail.com>
2017-05-04 11:02:23 +0100

    jogo do 31 adicionado para testes

--- galo.pl
+++ galo.pl
@@ -17,15 +17,15 @@ valor((V,L),0,_):- terminal((V,L)).
 %operadores de estado.
 %op1((O,Li),joga,(X,Lf)):-  inv(O,X),jogValida(X,Li,Lf).
 
-op1((O,Li),joga11,(X,Lf)):- member(((1,1),vazio),Li),replaceP(((1,1),vazio),((1,1),O),Li,Lf),inv(O,X).
-op1((O,Li),joga12,(X,Lf)):- member(((1,2),vazio),Li),replaceP(((1,2),vazio),((1,2),O),Li,Lf),inv(O,X).
-op1((O,Li),joga13,(X,Lf)):- member(((1,3),vazio),Li),replaceP(((1,3),vazio),((1,3),O),Li,Lf),inv(O,X).
-op1((O,Li),joga21,(X,Lf)):- member(((2,1),vazio),Li),replaceP(((2,1),vazio),((2,1),O),Li,Lf),inv(O,X).
-op1((O,Li),joga22,(X,Lf)):- member(((2,2),vazio),Li),replaceP(((2,2),vazio),((2,2),O),Li,Lf),inv(O,X).
-op1((O,Li),joga23,(X,Lf)):- member(((2,3),vazio),Li),replaceP(((2,3),vazio),((2,3),O),Li,Lf),inv(O,X).
-op1((O,Li),joga31,(X,Lf)):- member(((3,1),vazio),Li),replaceP(((3,1),vazio),((3,1),O),Li,Lf),inv(O,X).
-op1((O,Li),joga32,(X,Lf)):- member(((3,2),vazio),Li),replaceP(((3,2),vazio),((3,2),O),Li,Lf),inv(O,X).
-op1((O,Li),joga33,(X,Lf)):- member(((3,3),vazio),Li),replaceP(((3,3),vazio),((3,3),O),Li,Lf),inv(O,X).
+op1((O,Li),joga11,(X,Lf)):- member(((1,1),vazio),Li),replace(((1,1),vazio),((1,1),O),Li,Lf),inv(O,X).
+op1((O,Li),joga12,(X,Lf)):- member(((1,2),vazio),Li),replace(((1,2),vazio),((1,2),O),Li,Lf),inv(O,X).
+op1((O,Li),joga13,(X,Lf)):- member(((1,3),vazio),Li),replace(((1,3),vazio),((1,3),O),Li,Lf),inv(O,X).
+op1((O,Li),joga21,(X,Lf)):- member(((2,1),vazio),Li),replace(((2,1),vazio),((2,1),O),Li,Lf),inv(O,X).
+op1((O,Li),joga22,(X,Lf)):- member(((2,2),vazio),Li),replace(((2,2),vazio),((2,2),O),Li,Lf),inv(O,X).
+op1((O,Li),joga23,(X,Lf)):- member(((2,3),vazio),Li),replace(((2,3),vazio),((2,3),O),Li,Lf),inv(O,X).
+op1((O,Li),joga31,(X,Lf)):- member(((3,1),vazio),Li),replace(((3,1),vazio),((3,1),O),Li,Lf),inv(O,X).
+op1((O,Li),joga32,(X,Lf)):- member(((3,2),vazio),Li),replace(((3,2),vazio),((3,2),O),Li,Lf),inv(O,X).
+op1((O,Li),joga33,(X,Lf)):- member(((3,3),vazio),Li),replace(((3,3),vazio),((3,3),O),Li,Lf),inv(O,X).
 
 
 
@@ -100,7 +100,7 @@ tudoIgual([H|T],V):-
 	\+ V = vazio, 
 	tudoIgual(T,V).
 
-replaceP(_, _, [], []).
-replaceP(O, R, [O|T], [R|T2]) :- replaceP(O, R, T, T2).
-replaceP(O, R, [H|T], [H|T2]) :- \+H=O, replaceP(O, R, T, T2).
+replace(_, _, [], []).
+replace(O, R, [O|T], [R|T2]) :- replace(O, R, T, T2).
+replace(O, R, [H|T], [H|T2]) :- \+H=O, replace(O, R, T, T2).
 
